4d685e2da796b87e970438955ef23a937632d5b3
Kien Nguyen <kien.x.ng@gmail.com>
2022-05-13 15:22:36 -0500

    tent pl

---
+++ cifar/cifar10c_pl.py
@@ -0,0 +1,257 @@
+import logging
+
+import torch
+import torch.optim as optim
+
+from robustbench.data import load_cifar10c
+from robustbench.model_zoo.enums import ThreatModel
+from robustbench.utils import load_model
+from robustbench.utils import clean_accuracy as accuracy
+
+import math
+import json
+
+import tent
+import tent_ce
+import norm
+import cotta
+
+from conf import cfg, load_cfg_fom_args
+
+logger = logging.getLogger(__name__)
+
+
+def evaluate(description):
+    acc_history = {}
+    load_cfg_fom_args(description)
+    # configure model
+    base_model = load_model(cfg.MODEL.ARCH, cfg.CKPT_DIR,
+                       cfg.CORRUPTION.DATASET, ThreatModel.corruptions).cuda()
+    if cfg.MODEL.ADAPTATION == "source":
+        logger.info("test-time adaptation: NONE")
+        model = setup_source(base_model)
+    if cfg.MODEL.ADAPTATION == "norm":
+        logger.info("test-time adaptation: NORM")
+        model = setup_norm(base_model)
+    if cfg.MODEL.ADAPTATION == "tent":
+        logger.info("test-time adaptation: TENT")
+        model = setup_tent(base_model)
+    if cfg.MODEL.ADAPTATION == "tent_ce":
+        logger.info("test-time adaptation: TENT_CE")
+        model = setup_tent_ce(base_model)
+    if cfg.MODEL.ADAPTATION == "cotta":
+        logger.info("test-time adaptation: CoTTA")
+        model = setup_cotta(base_model)
+    # evaluate on each severity and type of corruption in turn
+    for severity in cfg.CORRUPTION.SEVERITY:
+        for i_c, corruption_type in enumerate(cfg.CORRUPTION.TYPE):
+            # continual adaptation for all corruption 
+            if i_c == 0:
+                try:
+                    model.reset()
+                    logger.info("resetting model")
+                except:
+                    logger.warning("not resetting model")
+            else:
+                logger.warning("not resetting model")
+            x_test, y_test = load_cifar10c(cfg.CORRUPTION.NUM_EX,
+                                           severity, cfg.DATA_DIR, False,
+                                           [corruption_type])
+            x_test, y_test = x_test.cuda(), y_test.cuda()
+            if cfg.MODEL.ADAPTATION == "tent_pl":
+                pseudos = pseudo_labels(model.model, x_test, cfg.TEST.BATCH_SIZE)
+                acc = accuracy_pl(model, x_test, y_test, pseudos, cfg.TEST.BATCH_SIZE)
+            else:
+                acc = accuracy(model, x_test, y_test, cfg.TEST.BATCH_SIZE)
+
+            # forward and backward transfer for continual learning
+            # save acc for current task T
+            # acc_history[i_c] = {}
+            # acc_history[i_c]['acc'] = acc
+            # if i_c > 0:
+            #     # and past tasks
+            #     for i, ct in enumerate(cfg.CORRUPTION.TYPE[:i_c]):
+            #         x_test, y_test = load_cifar10c(cfg.CORRUPTION.NUM_EX,
+            #                                severity, cfg.DATA_DIR, False,
+            #                                [ct])
+            #         x_test, y_test = x_test.cuda(), y_test.cuda()
+            #         if cfg.MODEL.ADAPTATION == "tent_ce":
+            #             past_acc = accuracy_ce(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
+            #         else:
+            #             past_acc = accuracy(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
+            #         acc_history[i_c][i] = past_acc
+            err = 1. - acc
+            logger.info(f"error % [{corruption_type}{severity}]: {err:.2%}")
+    # logger.info(json.dumps(acc_history, sort_keys=False, indent=4))
+    # calculate backward transfer
+    # num_tasks = len(list(acc_history.keys()))
+    # bwt = []
+    # for task in sorted(acc_history.keys()):
+    #     temp = 0.
+    #     for future in range(task, num_tasks):
+    #         temp += acc_history[future][task] - acc_history[task]['acc']
+    #     bwt.append(temp / (num_tasks - 1))
+    # logger.info(f'BWT: {(sum(bwt) / len(bwt)):.2}')
+
+    # calculate backward transfer
+    # num_tasks = len(list(acc_history.keys()))
+    # bwt = []
+    # for task in sorted(acc_history.keys()):
+    #     temp = 0.
+    #     for future in range(task, num_tasks - 1):
+    #         temp += acc_history[future][task] - acc_history[task]['acc']
+    #     bwt.append(temp / (num_tasks - 1))
+    # logger.info(f'BWT: {(sum(bwt) / len(bwt)):.2}')
+
+def pseudo_labels(model, x, batch_size, device=None):
+    if device is None:
+        device = x.device
+    labels = []
+    n_batches = math.ceil(x.shape[0] / batch_size)
+    with torch.no_grad():
+        for counter in range(n_batches):
+            x_curr = x[counter * batch_size:(counter + 1) * batch_size].to(device)
+            y_pred = model(x_curr)
+            y_pseudo = torch.argmax(y_pred, dim=1)
+            labels.append(y_pseudo)
+    return torch.cat(labels)
+
+def accuracy_ce(model, x, y, batch_size, device=None):
+    if device is None:
+        device = x.device
+    acc = 0.
+    n_batches = math.ceil(x.shape[0] / batch_size)
+    with torch.no_grad():
+        for counter in range(n_batches):
+            x_curr = x[counter * batch_size:(counter + 1) * batch_size].to(device)
+            y_curr = y[counter * batch_size:(counter + 1) * batch_size].to(device)
+
+            output = model(x_curr, y_curr)
+            acc += (output.max(1)[1] == y_curr).float().sum()
+
+    return acc.item() / x.shape[0]
+
+def accuracy_pl(model, x, y, pl, batch_size, device=None):
+    if device is None:
+        device = x.device
+    acc = 0.
+    n_batches = math.ceil(x.shape[0] / batch_size)
+    with torch.no_grad():
+        for counter in range(n_batches):
+            x_curr = x[counter * batch_size:(counter + 1) * batch_size].to(device)
+            y_curr = y[counter * batch_size:(counter + 1) * batch_size].to(device)
+            pl_curr = pl[counter * batch_size:(counter + 1) * batch_size].to(device)
+
+            output = model(x_curr, pl_curr)
+            acc += (output.max(1)[1] == y_curr).float().sum()
+
+    return acc.item() / x.shape[0]
+
+def setup_source(model):
+    """Set up the baseline source model without adaptation."""
+    model.eval()
+    logger.info(f"model for evaluation: %s", model)
+    return model
+
+
+def setup_norm(model):
+    """Set up test-time normalization adaptation.
+
+    Adapt by normalizing features with test batch statistics.
+    The statistics are measured independently for each batch;
+    no running average or other cross-batch estimation is used.
+    """
+    norm_model = norm.Norm(model)
+    logger.info(f"model for adaptation: %s", model)
+    stats, stat_names = norm.collect_stats(model)
+    logger.info(f"stats for adaptation: %s", stat_names)
+    return norm_model
+
+
+def setup_tent(model):
+    """Set up tent adaptation.
+
+    Configure the model for training + feature modulation by batch statistics,
+    collect the parameters for feature modulation by gradient optimization,
+    set up the optimizer, and then tent the model.
+    """
+    model = tent.configure_model(model)
+    params, param_names = tent.collect_params(model)
+    optimizer = setup_optimizer(params)
+    tent_model = tent.Tent(model, optimizer,
+                           steps=cfg.OPTIM.STEPS,
+                           episodic=cfg.MODEL.EPISODIC)
+    logger.info(f"model for adaptation: %s", model)
+    logger.info(f"params for adaptation: %s", param_names)
+    logger.info(f"optimizer for adaptation: %s", optimizer)
+    return tent_model
+
+def setup_tent_ce(model):
+    """Set up tent with cross-entropy adaptation.
+
+    Configure the model for training + feature modulation by batch statistics,
+    collect the parameters for feature modulation by gradient optimization,
+    set up the optimizer, and then tent the model.
+    """
+    model = tent_ce.configure_model(model)
+    params, param_names = tent_ce.collect_params(model)
+    optimizer = setup_optimizer(params)
+    tent_model = tent_ce.Tent(model, optimizer,
+                           steps=cfg.OPTIM.STEPS,
+                           episodic=cfg.MODEL.EPISODIC)
+    logger.info(f"model for adaptation: %s", model)
+    logger.info(f"params for adaptation: %s", param_names)
+    logger.info(f"optimizer for adaptation: %s", optimizer)
+    return tent_model
+
+def setup_cotta(model):
+    """Set up tent adaptation.
+
+    Configure the model for training + feature modulation by batch statistics,
+    collect the parameters for feature modulation by gradient optimization,
+    set up the optimizer, and then tent the model.
+    """
+    model = cotta.configure_model(model)
+    params, param_names = cotta.collect_params(model)
+    optimizer = setup_optimizer(params)
+    cotta_model = cotta.CoTTA(model, optimizer,
+                           steps=cfg.OPTIM.STEPS,
+                           episodic=cfg.MODEL.EPISODIC, 
+                           mt_alpha=cfg.OPTIM.MT, 
+                           rst_m=cfg.OPTIM.RST, 
+                           ap=cfg.OPTIM.AP)
+    logger.info(f"model for adaptation: %s", model)
+    logger.info(f"params for adaptation: %s", param_names)
+    logger.info(f"optimizer for adaptation: %s", optimizer)
+    return cotta_model
+
+
+def setup_optimizer(params):
+    """Set up optimizer for tent adaptation.
+
+    Tent needs an optimizer for test-time entropy minimization.
+    In principle, tent could make use of any gradient optimizer.
+    In practice, we advise choosing Adam or SGD+momentum.
+    For optimization settings, we advise to use the settings from the end of
+    trainig, if known, or start with a low learning rate (like 0.001) if not.
+
+    For best results, try tuning the learning rate and batch size.
+    """
+    if cfg.OPTIM.METHOD == 'Adam':
+        return optim.Adam(params,
+                    lr=cfg.OPTIM.LR,
+                    betas=(cfg.OPTIM.BETA, 0.999),
+                    weight_decay=cfg.OPTIM.WD)
+    elif cfg.OPTIM.METHOD == 'SGD':
+        return optim.SGD(params,
+                   lr=cfg.OPTIM.LR,
+                   momentum=cfg.OPTIM.MOMENTUM,
+                   dampening=cfg.OPTIM.DAMPENING,
+                   weight_decay=cfg.OPTIM.WD,
+                   nesterov=cfg.OPTIM.NESTEROV)
+    else:
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    evaluate('"CIFAR-10-C evaluation.')
