10c6488c0ed2847d78a00fcdb0a78aede0b943b7
Nyquixt <kxnguyen@udel.edu>
2022-07-15 09:19:16 -0400

    update

--- cifar/cifar10c.py
+++ cifar/cifar10c.py
@@ -43,6 +43,7 @@ def evaluate(description):
     if cfg.MODEL.ADAPTATION == "cotta":
         logger.info("test-time adaptation: CoTTA")
         model = setup_cotta(base_model)
+
     # evaluate on each severity and type of corruption in turn
     for severity in cfg.CORRUPTION.SEVERITY:
         for i_c, corruption_type in enumerate(cfg.CORRUPTION.TYPE):
@@ -64,44 +65,28 @@ def evaluate(description):
             else:
                 acc = accuracy(model, x_test, y_test, cfg.TEST.BATCH_SIZE)
 
-            # forward and backward transfer for continual learning
-            if i_c > 0:
-                # save acc for current task T
-                acc_history[corruption_type] = {}
-                acc_history[corruption_type]['acc'] = acc
-                # and past tasks
-                for i, ct in enumerate(cfg.CORRUPTION.TYPE[:i_c]):
-                    x_test, y_test = load_cifar10c(cfg.CORRUPTION.NUM_EX,
-                                           severity, cfg.DATA_DIR, False,
-                                           [ct])
-                    x_test, y_test = x_test.cuda(), y_test.cuda()
-                    if cfg.MODEL.ADAPTATION == "tent_ce":
-                        past_acc = accuracy_ce(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
-                    else:
-                        past_acc = accuracy(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
-                    acc_history[corruption_type][ct] = past_acc
             err = 1. - acc
             logger.info(f"error % [{corruption_type}{severity}]: {err:.2%}")
-    logger.info(json.dumps(acc_history, sort_keys=False, indent=4))
-
-# def softmax_entropy(x: torch.Tensor) -> torch.Tensor:
-#     """Entropy of softmax distribution from logits."""
-#     return -(x.softmax(1) * x.log_softmax(1)).sum(1)
-
-# def tent_adapt(model, optimizer, x, y, batch_size, device=None):
-#     if device is None:
-#         device = x.device
-#     n_batches = math.ceil(x.shape[0] / batch_size)
-
-#     for counter in range(n_batches):
-#         x_curr = x[counter * batch_size:(counter + 1) * batch_size].to(device)
-#         y_curr = y[counter * batch_size:(counter + 1) * batch_size].to(device)
-
-#         outputs = model(x_curr)
-#         loss = softmax_entropy(outputs).mean(0)
-#         loss.backward()
-#         optimizer.step()
-#         optimizer.zero_grad()
+            
+            # forward and backward transfer for continual learning
+    #         if i_c > 0:
+    #             # save acc for current task T
+    #             acc_history[corruption_type] = {}
+    #             acc_history[corruption_type]['acc'] = acc
+    #             # and past tasks
+    #             for i, ct in enumerate(cfg.CORRUPTION.TYPE[:i_c]):
+    #                 x_test, y_test = load_cifar10c(cfg.CORRUPTION.NUM_EX,
+    #                                        severity, cfg.DATA_DIR, False,
+    #                                        [ct])
+    #                 x_test, y_test = x_test.cuda(), y_test.cuda()
+    #                 if cfg.MODEL.ADAPTATION == "tent_ce":
+    #                     past_acc = accuracy_ce(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
+    #                 else:
+    #                     past_acc = accuracy(model.model, x_test, y_test, cfg.TEST.BATCH_SIZE)
+    #                 acc_history[corruption_type][ct] = past_acc
+    #         err = 1. - acc
+    #         logger.info(f"error % [{corruption_type}{severity}]: {err:.2%}")
+    # logger.info(json.dumps(acc_history, sort_keys=False, indent=4))
 
 def accuracy_ce(model, x, y, batch_size, device=None):
     if device is None:
